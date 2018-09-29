ca846bee8c23949d0f33ce5e518145f1a57abfb4
GKF <gekunfei@live.com>
2018-09-29 12:58:15 +0800

    去除白色背景

--- library/ColorPicker/src/main/java/cn/qqtheme/framework/popup/BasicPopup.java
+++ library/ColorPicker/src/main/java/cn/qqtheme/framework/popup/BasicPopup.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.annotation.CallSuper;
 import android.support.annotation.StyleRes;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
